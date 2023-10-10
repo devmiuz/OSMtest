5f5adf357f4782c370fb89013e92219bd61d3287
Mukhtorjon <Imi65imi57@gmail.com>
2023-10-10 19:49:04 +0500

    minor changes
    Took 47 minutes

--- app/src/main/java/uz/devmi/osmtest/MainActivity.kt
+++ app/src/main/java/uz/devmi/osmtest/MainActivity.kt
@@ -23,48 +23,47 @@ class MainActivity : AppCompatActivity() {
         val ctx = applicationContext
         Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx))
         map = findViewById<View>(R.id.map) as MapView
-        map.tileProvider.clearTileCache()
         Configuration.getInstance().cacheMapTileCount = 12.toShort()
         Configuration.getInstance().cacheMapTileOvershoot = 12.toShort()
 
-
+        map.tileProvider.clearTileCache()
         map.setTileSource(TileSourceFactory.MAPNIK)
-
         map.setMultiTouchControls(true)
         val mapController: IMapController = map.controller
         val startPoint = GeoPoint(41.29, 69.24)
         mapController.setZoom(12.0)
         mapController.setCenter(startPoint)
         map.invalidate()
+
         createmarker()
     }
 
     private fun createmarker() {
 
-        val originMarker = CustomMarker(
+        val originMarker = MarketMarker(
             mapView = map,
             identifier = "1111",
             title = "AAA",
             pointColor = Points.GREY,
             descriptionList = listOf("1 000 USD" to "#02E767", "500 000 UZS" to "#E66767")
         ) {
-
+            map.controller.animateTo(GeoPoint(41.20, 69.00))
+            Toast.makeText(this, "$it", Toast.LENGTH_SHORT).show()
         }
         originMarker.position = GeoPoint(41.30, 69.20)
         originMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
         originMarker.setPanToView(true)
-        originMarker.generateImage()
-
         map.overlays.add(originMarker)
 
-        val originMarker1 = CustomMarker(
+        val originMarker1 = MarketMarker(
             mapView = map,
             title = "BBB",
-            identifier = "1111",
+            identifier = "2222",
             pointColor = Points.GREEN,
             descriptionList = listOf("200 000 UZS" to "#02E767", "50 USD" to "#02E767")
         ) {
-
+            map.controller.animateTo(GeoPoint(41.20, 69.00))
+            Toast.makeText(this, "$it", Toast.LENGTH_SHORT).show()
         }
         originMarker1.position = GeoPoint(41.25, 69.10)
         originMarker1.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
@@ -72,16 +71,15 @@ class MainActivity : AppCompatActivity() {
         originMarker1.generateImage()
         map.overlays.add(originMarker1)
 
-        val originMarker2 = CustomMarker(
+        val originMarker2 = MarketMarker(
             mapView = map,
-            identifier = "2222",
+            identifier = "3333",
             title = "CCC",
             pointColor = Points.RED,
             descriptionList = listOf("200 000 UZS" to "#E66767", "500 000 USD" to "#E66767")
         ) {
             map.controller.animateTo(GeoPoint(41.20, 69.00))
             Toast.makeText(this, "$it", Toast.LENGTH_SHORT).show()
-
         }
         originMarker2.position = GeoPoint(41.20, 69.00)
         originMarker2.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
---
+++ app/src/main/java/uz/devmi/osmtest/Points.kt
@@ -0,0 +1,11 @@
+package uz.devmi.osmtest
+
+class Points {
+    companion object {
+        const val RED = "#e66767"
+        const val GREY = "#8c8c8c"
+        const val BLUE = "#6770e6"
+        const val GREEN = "#69e667"
+        const val NO_COLOR = ""
+    }
+}
